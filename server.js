b0bcb6eebbc1becdd3c7a6b356a72e511f7bd3ec
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-10-19 20:41:47 +0300

    deleted src folder and kept index.js in root folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // your fake database
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
